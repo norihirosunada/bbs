51e78eee6bb854c6b99f198e95115b18f5bfa9ad
norihirosunada <norisuna1201@gmail.com>
2020-02-09 11:47:17 +0000

    bbs 3/4, done

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_102358) do
+ActiveRecord::Schema.define(version: 2020_02_09_112632) do
 
   create_table "contributions", force: :cascade do |t|
     t.string "name"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "img"
   end
 
 end
